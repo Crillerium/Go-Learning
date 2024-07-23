a5b6e0208e81c949c114dd4e51780208400b65c9
Crillerium <crillerium@outlook.com>
2024-07-23 23:47:24 +0800

    update 04 with multi-action

--- 04/superpi.go
+++ 04/superpi.go
@@ -2,32 +2,33 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strconv"
 )
 
 func main () {
     var input string
-    fmt.Printf("请输入计算强度(1~100000000...):")
-    _ ,err := fmt.Scanln(&input)
-    if err != nil {
-        fmt.Println("错误,请输入有效的计算强度")
-        os.Exit(0)
-    }
-	num := 0.0
-	ber := 1.0
-	act := "add"
-	in,err := strconv.ParseFloat(input,64)
-	for ber <= float64(in) {
-		if act == "add" {
-			num += (1/ber)
-			ber += 2
-			act = "min"
-		}else if act == "min" {
-			num -= (1/ber)
-			ber += 2
-			act = "add"
-		}
+    for true {
+        fmt.Printf("请输入计算强度(1~100000000...):")
+        _ ,err := fmt.Scanln(&input)
+        if err != nil {
+            fmt.Println("错误,请输入有效的计算强度")
+            continue
+        }
+    	num := 0.0
+    	ber := 1.0
+    	act := "add"
+    	in,err := strconv.ParseFloat(input,64)
+    	for ber <= float64(in) {
+    		if act == "add" {
+    			num += (1/ber)
+    			ber += 2
+    			act = "min"
+    		}else if act == "min" {
+    			num -= (1/ber)
+    			ber += 2
+    			act = "add"
+    		}
+    	}
+    	fmt.Printf("圆周率的近似值计算得 %v\n",num*4)
 	}
-	fmt.Printf("圆周率的近似值计算得 %v\n",num*4)
 }
